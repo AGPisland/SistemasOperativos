06bc8d7b1516a5804a0c2e9423c06a40b68cab49
hardcore_hentai <klomilg@gmail.com>
2019-06-26 17:13:05 -0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include "procesos.cpp"
+#include "procesos2.cpp"
 #include <cstring>
 
 const string c_exit = "-exit";
@@ -12,20 +12,30 @@ const string c_ayuda = "-help";
 using namespace std;
 
 void ayuda();
+void avanzar();
 
 int main(){
 	
-	string user_op;
+	string user_op, log;
+	int user_num;
 	
 	cout<<"MULTIPROCESOS"<<endl;
 	system("pause");
+		
+	inicializar_rq();
 	
 	while(true){
 		system("cls");
-		cout<<"Cantidad de nucleos: "<<core_number<<endl;
-		cout<<"Cantidad de procesos de la ready Queue: "<<ready_q.size()<<endl;
+				
+		cout<<endl; imprimir_rq();cout<<endl;
+		
+		cout<<"Cantidad de procesos de la ready Queue: "<<get_p_count()<<endl;
 		cout<<"Cantidad de procesos en la wait Queue: "<<wait_q.size()<<endl;
 		cout<<endl<<">> Para ayuda, ingrese -help <<"<<endl<<endl;
+		
+		//listar procesos
+		
+		//user input
 		cout<<">> ";
 		cin>>user_op;
 		
@@ -43,6 +53,37 @@ int main(){
 		if(user_op == c_ayuda){
 			ayuda();
 			system("pause");
+			continue;
+		}
+		
+		//avanzar
+		if(user_op == c_avanzar){
+			av_procesos();
+			continue;
+		}
+		
+		//insertar sin avanzar (solo a la wait Q)
+		if(user_op == c_ins_no_adv){
+			cout<<">>Ingrese el Tiempo de Ejecucion del proceso: ";
+			cin>>user_num;
+			if(user_num<=0){user_num=1;}
+			ingresar_p(user_num);
+			continue;
+		}
+		
+		//insertar con avanzar
+		if(user_op == c_insertar){
+			//ingresar thread
+			cout<<">>Ingrese el Tiempo de Ejecucion del proceso: ";
+			cin>>user_num;
+			if(user_num<=0){user_num=1;}
+			ingresar_p(user_num);
+			
+			//llenar ready_Q
+			mover_procesos();
+			
+			//avanzar sistema
+			av_procesos();
 			continue;
 		}
 	}
@@ -63,3 +104,4 @@ void ayuda (){
 	cout<<c_ayuda<<" muestra la ayuda\n";
 	cout<<endl;
 }
+
--- test.cpp
+++ test.cpp
@@ -1,11 +1,23 @@
-#include "procesos.cpp"
+#include "procesos2.cpp"
+#include <cstring>
 
 using namespace std;
 
 int main(){
-	int c = core_number;
+	
+	ready_q[0].mod_id(3);
+	ready_q[1].mod_id(2);
+	ready_q[2].mod_id(5);
+	ready_q[3].mod_id(5);
+	ready_q[4].mod_id(0);
+	ready_q[5].mod_id(1);
+	ready_q[6].mod_id(5);
+	ready_q[7].mod_id(5);
+	
+	imprimir_rq();
+	
+	int c = get_rq_space();
 	cout<<c;
-	c=cambiar_core_count(4);
-	cout<<endl<<c;
+	
 	return 0;
 }
